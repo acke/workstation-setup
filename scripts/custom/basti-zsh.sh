c17bedf380cb51901029bae9085716007dd3cd94
Bastian Doetsch <bastian.doetsch@snyk.io>
2023-05-26 08:02:50 +0200

    chore: update .zshrc.local generation

--- scripts/custom/basti-zsh.sh
+++ scripts/custom/basti-zsh.sh
@@ -24,7 +24,7 @@ echo "Installing zsh-autosuggestions"
 brew install zsh-autosuggestions
 
 # configure zsh plugins
-PLUGINS="plugins=(git gitfast git-extras github pip kubectl docker golang gradle helm iterm2 mercurial mvn nmap npm python rust aws cloudfoundry common-aliases brew vscode)"
+PLUGINS="plugins=(git gitfast git-extras github pip kubectl docker golang gradle helm iterm2 mercurial mvn nmap npm python rust aws cloudfoundry common-aliases brew vscode gcloud)"
 rg --passthru -N "^plugins=.*" -r "$PLUGINS" ~/.zshrc > /tmp/.zshrc && mv /tmp/.zshrc ~/.zshrc
 
 
@@ -35,7 +35,11 @@ echo "source ~/.zshrc.local" >> ~/.zshrc
 
 cat <<EOF > ~/.zshrc.local
 DISABLE_UPDATE_PROMPT=true
-PATH=\$HOME/.cargo/bin:\$HOME/bin:/usr/local/bin:\${PATH}
+GOPATH=\$HOME/workspace/go
+GOROOT="\$(brew --prefix golang)/libexec"
+PATH=\$HOME/.cargo/bin:$HOME/bin:/usr/local/bin:\${PATH}
+PATH=\$GOROOT/bin:$PATH:$GOPATH/bin:\$HOME/pact/bin
+EDITOR='nvim'
 
 ASPATH=/usr/local/share/zsh-autosuggestions
 if [[ -f /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]]; then
@@ -43,16 +47,20 @@ if [[ -f /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]]; then
 fi
 source \$ASPATH/zsh-autosuggestions.zsh
 
-export EDITOR='nvim'
+export EDITOR
+export GITHUB_TOKEN
+export GOPATH
+export GOROOT
+export PATH
 
 alias git='git-together'
 alias git-pull-all='find . -type d -depth 1 -exec git -C {} pull \;'
 alias gpa=git-pull-all
-alias vi=vim
 alias ls='lsd'
 alias dir='ls -lah'
 alias z="zoxide"
 alias vi=nvim
+alias cat='bat -p'
 
 # disable FZF keybinding for history (ctrl-r) even if initialized in other file in favor for mcfly
 bindkey -r "^R"
@@ -62,7 +70,7 @@ rg --passthru -N "bindkey '\^R' fzf-history-widget" -r "# bindkey '^R' fzf-histo
 eval "\$(direnv hook zsh)"
 eval "\$(zoxide init zsh)"
 eval "\$(mcfly init zsh)"
-
+eval "\$(fnm env)"
 
 # add git-together pairing credentials to prompt
 function pairing_initials {
@@ -80,9 +88,23 @@ function pairing_initials {
 export PAIRING_INITIALS='%{%F{2}%}\$(pairing_initials)%{\${reset_color}%}'
 export PROMPT="\$PAIRING_INITIALS\$PROMPT"
 
-# this must be after the prompt command, as autojump uses the prompt to work
-[ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh
+source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
 
+test -d "\${GOPATH}" || mkdir "\${GOPATH}"
+test -d "\${GOPATH}/src/github.com" || mkdir -p "\${GOPATH}/src/github.com"
+
+# git commit signing
+export GPG_TTY=\$(tty)
+SECRET_KEY=\$(gpg --list-secret-keys --keyid-format=long|head -4|tail -1|xargs)
+CONFIGURED_SIGNING_KEY=\$(git config --global user.signingkey)
+cd ~/workspace/workstation-setup/
+if [[ \$SECRET_KEY != \$CONFIGURED_SIGNING_KEY ]]
+then
+  echo "Configuring git signing key"
+  git config --global user.signingkey \$SECRET_KEY
+fi
+cd -
 EOF
 cp "${WORK_DIR}"/scripts/custom/basti.zsh-theme "${OH_DIR}"/themes/
 sed -i '' -e "s/ZSH_THEME=\".*$/ZSH_THEME=\"basti\"/g" ~/.zshrc
